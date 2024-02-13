196300a7cee5163f6a429e85402346496d81820d
nomanSanjari <nomansanjari2001@gmail.com>
2024-02-13 06:12:37 -0700

    Container element styled and complete with font
    Signed-off-by: nomanSanjari <nomansanjari2001@gmail.com>

--- mod-man-frontend/src/App.jsx
+++ mod-man-frontend/src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { Container } from "./components/styled/Container.styled";
 
 function App() {
-	return <></>;
+	return (
+		<Container>
+			<h1>Hello, World!</h1>
+		</Container>
+	);
 }
 
 export default App;
---
+++ mod-man-frontend/src/components/styled/Container.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #212124;
+	color: white;
+	font-family: "Source Code Pro", monospace;
+	font-optical-sizing: auto;
+	font-weight: 400;
+	font-style: normal;
+`;
