10d50c410ee8f04fde8cd1a18fc9aa653999b813
nomanSanjari <nomansanjari2001@gmail.com>
2024-02-15 08:11:21 -0700

    Terraform setup
    Signed-off-by: nomanSanjari <nomansanjari2001@gmail.com>

---
+++ infrastructure/main.tf
@@ -0,0 +1,18 @@
+# Terraform configuration
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+
+
